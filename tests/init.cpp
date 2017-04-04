1866e8aab777a5238a460b12784add234a25f66c
kate-lozovaya <katekalozovaya@gmail.com>
2017-04-05 00:47:31 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,8 +1,58 @@
 #include <vector.hpp>
 #include <catch.hpp>
 
-SCENARIO("vector init", "[init]") {
+SCENARIO("default constructor") 
+{
 	vector_t vector;
 	REQUIRE(vector.size() == 0);
 	REQUIRE(vector.capacity() == 0);
 }
+
+SCENARIO("constructor with params") 
+{
+	vector_t vector(1);
+	REQUIRE(vector.size() == 1);
+	REQUIRE(vector.capacity() == 1);
+	REQUIRE(vector[0] == 0);
+}
+
+SCENARIO("copy constructor") 
+{
+	vector_t vector1(1);
+	vector_t vector2(vector1);
+	REQUIRE(vector1.size() == vector2.size());
+	REQUIRE(vector1.capacity() == vector2.capacity());
+	REQUIRE(vector1[] == vector2[]);
+}
+
+SCENARIO("=") 
+{
+	vector_t vector1(1);
+	vector_t vector2 = vector1;
+	REQUIRE(vector1.size() == vector2.size());
+	REQUIRE(vector1.capacity() == vector2.capacity());
+	REQUIRE(vector1[] == vector2[]);
+}
+
+SCENARIO("push")
+{
+	vector_t vector(1);
+	vector.push_back(2);
+	vector.push_back(3);
+	REQUIRE(vector.size() == 3);
+	REQUIRE(vector.capacity() == 4);
+	REQUIRE(vector[0] == 0);
+	REQUIRE(vector[1] == 2);
+	REQUIRE(vector[2] == 3);
+}
+
+SCENARIO("[]")
+{
+	vector_t vector(2);
+	REQUIRE(vector.size() == 2);
+	REQUIRE(vector.capacity() == 2);
+	REQUIRE(vector[0] == 0);
+	REQUIRE(vector[1] == 0);
+}
+
+
