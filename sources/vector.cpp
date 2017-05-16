b037694290523c5a258d2959862b8ba21d7e9e36
kate-lozovaya <katekalozovaya@gmail.com>
2017-05-16 14:04:10 +0400

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -66,27 +66,20 @@ auto vector_t::push_back(int value) -> void
 	size_++;
 }
 
-auto vector_t::del(int value) -> void
+auto vector_t::del() -> void
 {
-	int j = 0;
 	if (size_ != 0)
 	{
-		for (int i = 0; i < size_; ++i)
-		{
-			if (ptr_[i] == value)
-				j = i;
-		}
-		if ((size_ - 1) == (capacity_ / 2))
+		if (size_ - 1 == capacity_ /2)
 			capacity_ /= 2;
 		int* ptr = new int[capacity_];
-		for (int i = 0; i < j; ++i)
+		for (int i = 0; i < size_ - 1; ++i)
 			ptr[i] = ptr_[i];
-		for (int i = j; i < (size_ - 1); ++i)
-			ptr[i] = ptr_[j + 1];
 		delete[] ptr_;
 		ptr_ = ptr;
 		size_--;
 	}
+	else return;
 }
 
 auto vector_t::operator[](unsigned int index) const noexcept -> int
